66c10e573d6cd4fb1b6375cf5c12d774275f6526
Kenta Matsunaga <kmatsuna@c1r1s3.42tokyo.jp>
2024-09-05 16:59:43 +0900

    test2

--- ft_treat_something.c
+++ ft_treat_something.c
@@ -6,8 +6,17 @@
 /*   By: kmatsuna <kmatsuna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 16:29:41 by kmatsuna          #+#    #+#             */
-/*   Updated: 2024/09/05 16:29:54 by kmatsuna         ###   ########.fr       */
+/*   Updated: 2024/09/05 16:45:39 by kmatsuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
+
+int	ft_treat_something(int c, va_list args)
+{
+	int len;
+
+	len = 0;
+	if (c == 'c')
+		len = ft_treat_char(va_arg(args, int));
+}
